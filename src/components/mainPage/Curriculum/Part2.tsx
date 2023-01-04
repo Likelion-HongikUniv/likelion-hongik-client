d9a0074db1af4c8aa6264b3f3a80b34c70bc85e7
ch9eri <limch9eri@gmail.com>
2023-01-04 17:30:02 +0900

    feat : 타임라인 진행바

--- src/components/mainPage/Curriculum/Part2.tsx
+++ src/components/mainPage/Curriculum/Part2.tsx
@@ -5,7 +5,28 @@ const Part2 = () => {
   return (
     <Box>
       <PartName>프론트엔드 타임라인</PartName>
-      <div></div>
+      <BarContainer>
+        <Dot1>
+          <Date>2022.02.21 - 03.02</Date>
+          <Text>멋사 11기 지원 기간</Text>
+        </Dot1>
+        <Dot2>
+          <Date>2022.02.21 - 03.02</Date>
+          <Text>프론트엔드</Text>
+        </Dot2>
+        <Dot3>
+          <Date>2022.02.21 - 03.02</Date>
+          <Text>프론트엔드</Text>
+        </Dot3>
+        <Dot4>
+          <Date>2022.02.21 - 03.02</Date>
+          <Text>프론트엔드</Text>
+        </Dot4>
+        <Dot5>
+          <Date>2022.02.21 - 03.02</Date>
+          <Text>프론트엔드</Text>
+        </Dot5>
+      </BarContainer>
     </Box>
   );
 };
@@ -19,6 +40,59 @@ const Box = styled.div`
 const PartName = styled.h2`
   color: white;
   font-size: 28px;
+  margin-bottom: 40px;
+`;
+
+const BarContainer = styled.div`
+  width: 1210px;
+  height: 20px;
+  background: rgba(51, 51, 51, 0.8);
+  border-radius: 30px;
+  position: relative;
+`;
+
+const Dot1 = styled.div`
+  position: absolute;
+  width: 20px;
+  height: 20px;
+  left: 0px;
+  border-radius: 50px;
+  background: #d9d9d9;
+`;
+
+const Dot2 = styled(Dot1)`
+  position: absolute;
+  left: 250px;
+`;
+
+const Dot3 = styled(Dot2)`
+  left: 500px;
+`;
+
+const Dot4 = styled(Dot3)`
+  left: 750px;
+`;
+
+const Dot5 = styled(Dot4)`
+  left: 1000px;
+`;
+
+const Date = styled.div`
+  margin-top: 40px;
+  width: 149px;
+  height: 19px;
+  font-weight: 700;
+  font-size: 16px;
+  line-height: 19px;
+`;
+
+const Text = styled.div`
+  width: 182.68px;
+  height: 29px;
+  font-weight: 500;
+  font-size: 24px;
+  line-height: 29px;
+  margin-top: 16px;
 `;
 
 export default Part2;
