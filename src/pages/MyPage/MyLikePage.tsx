e895fc1b811fa6178493d90ed216289189c38e9c
임채리 <limch9eri@google.com>
2023-02-06 22:20:08 +0900

    fix: 마이페이지 프로필 사진 추가, 사이드바 recoil persist 추가

--- src/pages/MyPage/MyLikePage.tsx
+++ src/pages/MyPage/MyLikePage.tsx
@@ -9,12 +9,12 @@ import useMediaQuery from "../../hooks/useMediaQuery";
 import { MyPageMobileNav } from "../../components/elements/MyPageMobileNav";
 import MyPagination from "../MyPage/MyPagination";
 import { useRecoilState } from "recoil";
-import { currPageState } from "../../states/index";
+import { currPageState, profileImgState } from "../../states/index";
 
 interface IPost {
   title: string;
   author: string;
-  profileImage?: string;
+  profileImg?: string;
   body: string;
   time: string;
   likes: number;
@@ -28,6 +28,7 @@ export function MyLikePage() {
   const token = localStorage.getItem("token");
   const [currPage] = useRecoilState(currPageState);
   const [totalPages, setTotalPages] = useState(5);
+  const [profileImg] = useRecoilState(profileImgState);
 
   useEffect(() => {
     window.scrollTo(0, 0);
@@ -77,7 +78,7 @@ export function MyLikePage() {
                   likes={post.likes}
                   reply={post.reply}
                   time={post.time}
-                  profileImage={post.profileImage}
+                  profileImg={profileImg}
                 />
               ))}
             </PostItemContainer>
