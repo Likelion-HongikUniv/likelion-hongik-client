c75cc388876a79092ddc3656a564eaf310aa9d75
naro-Kim <gracias9022@gmail.com>
2023-02-19 22:26:26 +0900

    merge : post page error

--- src/pages/PostPage.tsx
+++ src/pages/PostPage.tsx
@@ -9,94 +9,44 @@ import { useEffect, useState } from "react";
 import { IComment, IBoard } from "../interfaces/comments";
 import { commentsListState, boardState } from "../states/atoms";
 import styled from "styled-components";
-import axios from "axios";
 import useMediaQuery from "../hooks/useMediaQuery";
-import { getPostDetail } from "../api/getPostDetail";
+import { useGetPostDetail } from "../api/useGetPostDetail";
 import { useQuery } from "@tanstack/react-query";
 
 export function PostPage() {
-  const [board, setBoardData] = useState<IBoard>({
-    postId: 1,
-    author: {
-      authorId: 1,
-      nickname: "",
-      profileImage: "",
-      isAuthor: true,
-    },
-    title: "",
-    body: "",
-    createdTime: "",
-    isLiked: true,
-    likeCount: 0,
-    comments: [],
-    imageUrls: [],
-  });
-  const [comments, setCommentsData] = useState<IComment[]>([
-    {
-      commentId: 1,
-      author: {
-        authorId: 1,
-        nickname: "",
-        profileImage: "",
-        isAuthor: true,
-      },
-      body: "",
-      isDeleted: false,
-      createdTime: "",
-      isLiked: false,
-      likeCount: 0,
-      replies: [],
-    },
-  ]);
   const isPC = useMediaQuery("(min-width: 1024px)");
   const isTablet = useMediaQuery("(min-width: 768px) and (max-width: 1024px)");
   const isMobile = useMediaQuery("(max-width : 768px)");
   const { id } = useParams<{ id?: string }>();
-
   const postIdToNumber = Number(id);
-  const { isLoading: isPostDataLoading, data: postData } = useQuery(["post-detail", postIdToNumber], () =>
-    getPostDetail(postIdToNumber),
-  );
-
-  useEffect(() => {
-    if (!isPostDataLoading && postData) {
-      console.log(postData);
-
-      setBoardData(postData);
-      setCommentsData(postData.comments);
-    }
-  }, [isPostDataLoading]);
-  // if (!isPostDataLoading && postData) {
-  //   setBoardData(postData);
-  //   setCommentsData(postData.comments);
-  // }
+  const { board, status } = useGetPostDetail(postIdToNumber);
 
   return (
     <>
       <Header />
-      {isPC && !isPostDataLoading && (
+      {isPC && board && (
         <Section style={{ padding: "0 340px", display: "flex", justifyContent: "center" }}>
           <Column style={{ marginTop: "100px" }}>
             <Board {...board} />
-            <CommentsList {...comments} />
+            <CommentsList {...board.comments} />
           </Column>
         </Section>
       )}
-      {isTablet && !isPostDataLoading && (
+      {isTablet && board && status === "success" && (
         <Section style={{ padding: "0 40px", display: "flex", justifyContent: "center" }}>
           <Column style={{ marginTop: "100px" }}>
             <Board {...board} />
             <Hairline />
-            <CommentsList {...comments} />
+            <CommentsList {...board.comments} />
           </Column>
         </Section>
       )}
-      {isMobile && !isPostDataLoading && (
+      {isMobile && board && status === "success" && (
         <Section style={{ position: "relative", padding: "0 20px", display: "flex", justifyContent: "center" }}>
           <Column style={{ marginTop: "100px" }}>
             <Board {...board} />
             <Hairline />
-            <CommentsList {...comments} />
+            <CommentsList {...board.comments} />
           </Column>
         </Section>
       )}
