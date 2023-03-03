c1ea54d0ad4dd79d123e6c1c793d288c7eeba3b2
yjoonjang <yjoonjang34@gmail.com>
2023-03-03 16:02:49 +0900

    fix: 배포 시 오류 해결

--- src/components/mainPage/Curriculum/Part1.tsx
+++ src/components/mainPage/Curriculum/Part1.tsx
@@ -8,27 +8,27 @@ const Part1 = () => {
       <S.BarContainer>
         <S.Dot1>
           <S.Date>1차 세션</S.Date>
-          <S.Text>Github 교육</S.Text>
+          <S.Text>GitHub</S.Text>
         </S.Dot1>
         <S.Dot2>
           <S.Date>2차 세션</S.Date>
-          <S.Text>기획디자인</S.Text>
+          <S.Text>기획자란?</S.Text>
         </S.Dot2>
         <S.Dot3>
           <S.Date>3차 세션</S.Date>
-          <S.Text>기획디자인</S.Text>
+          <S.Text>프로덕트 기획법</S.Text>
         </S.Dot3>
         <S.Dot4>
           <S.Date>4차 세션</S.Date>
-          <S.Text>기획디자인</S.Text>
+          <S.Text>PM이란?</S.Text>
         </S.Dot4>
         <S.Dot5>
           <S.Date>5차 세션</S.Date>
-          <S.Text>기획디자인</S.Text>
+          <S.Text>프로젝트 진행법</S.Text>
         </S.Dot5>
         <S.Dot6>
           <S.Date>6차 세션</S.Date>
-          <S.Text>기획디자인</S.Text>
+          <S.Text>협업 기법</S.Text>
         </S.Dot6>
       </S.BarContainer>
     </S.Box>
--- src/components/mainPage/Curriculum/Part2.tsx
+++ src/components/mainPage/Curriculum/Part2.tsx
@@ -8,27 +8,27 @@ const Part2 = () => {
       <S.BarContainer>
         <S.Dot1>
           <S.Date>1차 세션</S.Date>
-          <S.Text>Github 교육</S.Text>
+          <S.Text>GitHub</S.Text>
         </S.Dot1>
         <S.Dot2>
           <S.Date>2차 세션</S.Date>
-          <S.Text>HTML/CSS 교육</S.Text>
+          <S.Text>html/css</S.Text>
         </S.Dot2>
         <S.Dot3>
           <S.Date>3차 세션</S.Date>
-          <S.Text>Javascript 교육(1)</S.Text>
+          <S.Text>Javascript 기초</S.Text>
         </S.Dot3>
         <S.Dot4>
           <S.Date>4차 세션</S.Date>
-          <S.Text>Javascript 교육(2)</S.Text>
+          <S.Text>Javascript 심화</S.Text>
         </S.Dot4>
         <S.Dot5>
           <S.Date>5차 세션</S.Date>
-          <S.Text>REACT 교육</S.Text>
+          <S.Text>React 기초</S.Text>
         </S.Dot5>
         <S.Dot6>
-          <S.Date>2022.02.21 - 03.02</S.Date>
-          <S.Text>세션</S.Text>
+          <S.Date>6차 세션</S.Date>
+          <S.Text>styled-components & React Hooks</S.Text>
         </S.Dot6>
       </S.BarContainer>
     </S.Box>
